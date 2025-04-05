47df6fd5c47ef1a4536dabd8609b85505c53c271
Tomas Mikula <tomas.mikula@gmail.com>
2025-04-05 23:26:37 +0100

    Remove superfluous stuff from TestApp.

--- jing-openapi-examples/src/main/scala/jing/openapi/examples/TestApp.scala
+++ jing-openapi-examples/src/main/scala/jing/openapi/examples/TestApp.scala
@@ -1,21 +1,11 @@
 package jing.openapi.examples
 
 import jing.openapi.client.default.given
-import jing.openapi.model.Value
 import jing.openapi.model.Value.obj
 
 object TestApp extends App {
 
   val api = jing.openapi("https://petstore3.swagger.io/api/v3/openapi.json")
-
-  import api.schemas.Pet
-
-  private def petRewrap(x: Value[Pet]): Value[Pet] =
-    x match
-      case Pet(y) => Pet(y)
-
-  println(Pet.schema)
-  println()
 
   val result =
     api
