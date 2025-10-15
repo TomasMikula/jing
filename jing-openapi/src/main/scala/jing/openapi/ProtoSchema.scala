c872b09197acc014562021a0e6e29430273333df
Tomas Mikula <tomas.mikula@gmail.com>
2025-10-15 23:32:23 +0100

    Fuse resolveAcyclic and sortTopologically, thereby avoiding some illegal states. Remove ProtoSchema.Oriented.

--- jing-openapi/src/main/scala/jing/openapi/ProtoSchema.scala
+++ jing-openapi/src/main/scala/jing/openapi/ProtoSchema.scala
@@ -10,44 +10,36 @@ private[openapi] enum ProtoSchema {
   case Ref(schemaName: String)
   case Unsupported(details: String)
 
+  import ProtoSchema.*
+
   def resolve(
     alreadyResolved: Map[String, Schema.Labeled[String, ?]],
   ): Schema.Labeled[String, ?] =
-    this match
-      case Proper(value) =>
-        Schema.Labeled.Unlabeled:
-          value.wipeTranslate[Schema.Labeled[String, _]]([X] => ps => Exists(ps.resolve(alreadyResolved)))
-      case Ref(schemaName) =>
-        alreadyResolved.get(schemaName) match
-          case Some(schema) =>
-            Schema.Labeled.WithLabel(schemaName, schema)
-          case None =>
-            val msg = s"Unresolved schema $schemaName"
-            Schema.Labeled.Unsupported(SingletonType(msg))
-      case Unsupported(details) =>
-          Schema.Labeled.Unsupported(SingletonType(details))
-
-  import ProtoSchema.*
+    resolveA[[x] =>> x](using
+      Resolver.fromMap(alreadyResolved, notFound = Option.empty[Cycle])
+    )
 
   private def resolveA[F[_]](using
-    r: Resolver[ProtoSchema.Oriented, Option[Cycle], F],
+    r: Resolver[Schema.Labeled[String, ?], Option[Cycle], F],
     F: Applicative[F],
-  ): F[ProtoSchema.Oriented] =
+  ): F[Schema.Labeled[String, ?]] =
     this match
       case Proper(value) =>
         value
-          .wipeTranslateA[F, [x] =>> ProtoSchema.Oriented]([X] => ps => ps.resolveA[F].map(Exists(_)))
-          .map(Oriented.Proper(_))
+          .wipeTranslateA[F, Schema.Labeled[String, _]]([X] => ps => ps.resolveA[F].map(Exists(_)))
+          .map(Schema.Labeled.Unlabeled(_))
       case Ref(schemaName) =>
         r.resolve(schemaName) map:
           case Right(schema) =>
-            Oriented.BackwardRef(schemaName)
+            Schema.Labeled.WithLabel(schemaName, schema)
           case Left(None) =>
-            Oriented.Unsupported(s"Unresolved schema $schemaName")
+            val msg = s"Unresolved schema $schemaName"
+            Schema.Labeled.Unsupported(SingletonType(msg))
           case Left(Some(Cycle(cycle))) =>
-            Oriented.ForwardRef(schemaName, cycle)
+            val msg = s"Unsupported recursive schema: ${cycle.mkString(" -> ")}"
+            Schema.Labeled.Unsupported(SingletonType(msg))
       case Unsupported(details) =>
-        F.pure(Oriented.Unsupported(details))
+        F.pure(Schema.Labeled.Unsupported(SingletonType(details)))
 }
 
 private[openapi] object ProtoSchema {
@@ -90,52 +82,27 @@ private[openapi] object ProtoSchema {
     Proper(go(motif.Object.empty, props))
   }
 
-  /** Schema with unresolved references categorized as backward or forward,
-   *  resulting from topological sort (where presence of forward references
-   *  indicates cyclic references).
-   */
-  private enum Oriented {
-    case Proper(value: SchemaMotif[[A] =>> ProtoSchema.Oriented, ?])
-    case BackwardRef(schemaName: String)
-    case ForwardRef(schemaName: String, cycle: List[String])
-    case UnresolvedRef(schemaName: String)
-    case Unsupported(details: String)
-
-    private[ProtoSchema] def resolve(
-      alreadyResolved: Map[String, Schema.Labeled[String, ?]],
-    ): Schema.Labeled[String, ?] =
-      this match
-        case Oriented.Proper(value) =>
-          Schema.Labeled.Unlabeled:
-            value.wipeTranslate[Schema.Labeled[String, _]]([X] => pso => Exists(pso.resolve(alreadyResolved)))
-        case BackwardRef(schemaName) =>
-          alreadyResolved.get(schemaName) match
-            case Some(schema) =>
-              Schema.Labeled.WithLabel(schemaName, schema)
-            case None =>
-              throw new NoSuchElementException(s"Schema '$schemaName' not found among previously resolved schemas. This is a bug in JING, as it was previously determined to be a backward reference.")
-        case ForwardRef(schemaName, cycle) =>
-          val msg = s"Unsupported recursive schema: ${cycle.mkString(" -> ")}"
-          Schema.Labeled.Unsupported(SingletonType(msg))
-        case UnresolvedRef(schemaName) =>
-          val msg = s"Unresolved schema $schemaName"
-          Schema.Labeled.Unsupported(SingletonType(msg))
-        case Oriented.Unsupported(details) =>
-          Schema.Labeled.Unsupported(SingletonType(details))
-  }
+  private case class Cycle(schemas: List[String])
 
   private trait Resolver[A, E, F[_]] {
     def resolve(name: String): F[Either[E, A]]
   }
 
-  private type TopoSortF[I, O, A] = ReaderState[List[String], (List[(String, O)], List[(String, I)]), A]
+  private object Resolver {
+    def fromMap[A, NotFound](
+      m: Map[String, A],
+      notFound: NotFound,
+    ): Resolver[A, NotFound, [x] =>> x] =
+      new Resolver[A, NotFound, [x] =>> x]:
+        override def resolve(name: String): Either[NotFound, A] =
+          m.get(name).toRight(left = notFound)
+  }
 
-  private case class Cycle(schemas: List[String])
+  private type TopoSortF[I, O, A] = ReaderState[List[String], (List[(String, O)], List[(String, I)]), A]
 
   private object TopoSortF {
     def resolver[I, O](
       elemResolver: I => TopoSortF[I, O, O],
-      reportCycle: (String, List[String]) => O,
     ): Resolver[O, Option[Cycle], TopoSortF[I, O, _]] =
       new Resolver[O, Option[Cycle], TopoSortF[I, O, _]] {
         override def resolve(name: String): TopoSortF[I, O, Either[Option[Cycle], O]] =
@@ -160,24 +127,24 @@ private[openapi] object ProtoSchema {
               }
       }
 
-    val protoResolver: Resolver[ProtoSchema.Oriented, Option[Cycle], TopoSortF[ProtoSchema, ProtoSchema.Oriented, _]] =
-      new Resolver[ProtoSchema.Oriented, Option[Cycle], TopoSortF[ProtoSchema, ProtoSchema.Oriented, _]] { self =>
-        val elemResolver: ProtoSchema => TopoSortF[ProtoSchema, ProtoSchema.Oriented, ProtoSchema.Oriented] =
+    val protoResolver: Resolver[Schema.Labeled[String, ?], Option[Cycle], TopoSortF[ProtoSchema, Schema.Labeled[String, ?], _]] =
+      new Resolver[Schema.Labeled[String, ?], Option[Cycle], TopoSortF[ProtoSchema, Schema.Labeled[String, ?], _]] { self =>
+        val elemResolver: ProtoSchema => TopoSortF[ProtoSchema, Schema.Labeled[String, ?], Schema.Labeled[String, ?]] =
           _.resolveA(using self)
 
-        val delegate = resolver(elemResolver, reportCycle = Oriented.ForwardRef(_, _))
+        val delegate = resolver(elemResolver)
 
-        override def resolve(name: String): TopoSortF[ProtoSchema, Oriented, Either[Option[Cycle], Oriented]] =
+        override def resolve(name: String): TopoSortF[ProtoSchema, Schema.Labeled[String, ?], Either[Option[Cycle], Schema.Labeled[String, ?]]] =
           delegate.resolve(name)
       }
   }
 
-  private def sortTopologically(schemas: List[(String, ProtoSchema)]): List[(String, ProtoSchema.Oriented)] = {
+  def resolveAcyclic(schemas: List[(String, ProtoSchema)]): List[(String, Schema.Labeled[String, ?])] = {
     @tailrec
     def go(
-      acc: List[(String, Oriented)],
+      acc: List[(String, Schema.Labeled[String, ?])],
       remaining: List[(String, ProtoSchema)]
-    ): List[(String, Oriented)] =
+    ): List[(String, Schema.Labeled[String, ?])] =
       remaining match {
         case Nil =>
           acc
@@ -190,23 +157,6 @@ private[openapi] object ProtoSchema {
       }
 
     go(acc = Nil, remaining = schemas)
-  }
-
-  def resolveAcyclic(schemas: List[(String, ProtoSchema)]): List[(String, Schema.Labeled[String, ?])] = {
-    @tailrec
-    def go(
-      revAcc: List[(String, Schema.Labeled[String, ?])],
-      accMap: Map[String, Schema.Labeled[String, ?]], // contains the same elements as revAcc
-      remaining: List[(String, ProtoSchema.Oriented)],
-    ): List[(String, Schema.Labeled[String, ?])] =
-      remaining match
-        case (name, schema) :: tail =>
-          val resolved = schema.resolve(accMap)
-          go((name, resolved) :: revAcc, accMap.updated(name, resolved), tail)
-        case Nil =>
-          revAcc.reverse
-
-    go(Nil, Map.empty, sortTopologically(schemas))
   }
 
 }
