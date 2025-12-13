c18b3bb4b4f654d55bfab1a7acc30df62ff06b66
Tomas Mikula <tomas.mikula@gmail.com>
2025-12-13 18:52:45 +0000

    Remove the now unused Substitutive typeclass.

--- jing-openapi/src/main/scala/jing/openapi/Unrelated.scala
+++ jing-openapi/src/main/scala/jing/openapi/Unrelated.scala
@@ -6,12 +6,6 @@ private[openapi] object Unrelated {
   def apply[A, B](): Unrelated[A, B] =
     ()
 
-  given Substitutive[Unrelated] with {
-    override def refl[A]: Unrelated[A, A] = ()
-    override def trans[A, B, C](r: Unrelated[A, B], s: Unrelated[B, C]): Unrelated[A, C] = ()
-    override def subst[F[_], A, B](rel: Unrelated[A, B]): Unrelated[F[A], F[B]] = ()
-  }
-
   given Compatible[Unrelated] {
       import jing.openapi.model.*
       import libretto.lambda.util.SingletonType
