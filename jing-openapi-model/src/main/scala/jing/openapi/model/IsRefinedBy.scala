65b3b65335aa4ff778aaefca65f920d521b8b2bd
Tomas Mikula <tomas.mikula@gmail.com>
2025-12-25 00:00:40 +0000

    Remove IsRefinedBy.Trans constructor.
    Should composition be needed, it is implementable by normalization, using the other constructors.

--- jing-openapi-model/src/main/scala/jing/openapi/model/IsRefinedBy.scala
+++ jing-openapi-model/src/main/scala/jing/openapi/model/IsRefinedBy.scala
@@ -1,15 +1,13 @@
 package jing.openapi.model
 
 import jing.openapi.model.*
-import libretto.lambda.util.{Exists, SingletonType, TypeEq}
+import libretto.lambda.util.{Exists, SingletonType}
 import libretto.lambda.util.Exists.Indeed
-import libretto.lambda.util.TypeEq.Refl
 
 private[openapi] infix sealed trait IsRefinedBy[A, B]
 
 private[openapi] object IsRefinedBy {
   case class UnknownRefinedByAnything[T]() extends (Unknown IsRefinedBy T)
-  case class Trans[A, B, C](aRb: A IsRefinedBy B, bRc: B IsRefinedBy C) extends (A IsRefinedBy C)
 
   case class Lift_||[A, B, X, Y](aRb: A IsRefinedBy B, xRy: X IsRefinedBy Y) extends (A || X IsRefinedBy B || Y)
   case class LiftEnum[A, B, Cs, Ds](aRb: A IsRefinedBy B, cRd: Cs IsRefinedBy Ds) extends (Enum[A, Cs] IsRefinedBy Enum[B, Ds])
@@ -77,83 +75,68 @@ private[openapi] object IsRefinedBy {
       r match
         case r: Lift_||[a, b, x, y] =>
           Indeed(Indeed((summon[T =:= (b || y)], r.aRb, r.xRy)))
-        case Trans(r, s) =>
-          r.preserves_|| match
-            case Indeed(Indeed((ev1, r1, r2))) =>
-              ev1.substituteCo[IsRefinedBy[_, T]](s).preserves_|| match
-                case ex1 @ Indeed(ex2 @ Indeed((TypeEq(Refl()), s1, s2))) =>
-                  Indeed(Indeed((summon[T =:= (ex1.T || ex2.T)], Trans(r1, s1), Trans(r2, s2))))
 
   extension [K, V, T](r: (K :: V) IsRefinedBy T)
     def preserves_-:: : Exists[[W] =>> (T =:= (K :: W), V IsRefinedBy W)] =
       r match
         case r: Lift_::[k, v, w] =>
           Indeed((summon[T =:= (K :: w)], r.aRb))
-        case Trans(r, s) =>
-          ???
 
   extension [K, V, T](r: (K :? V) IsRefinedBy T)
     def preserves_-:? : Exists[[W] =>> (T =:= (K :? W), V IsRefinedBy W)] =
       r match
         case r: Lift_:?[k, v, w] =>
           Indeed((summon[T =:= (K :? w)], r.aRb))
-        case Trans(r, s) =>
-          ???
 
   extension [Ps, T](r: Obj[Ps] IsRefinedBy T)
     def preservesObj: Exists[[Qs] =>> (T =:= Obj[Qs], Ps IsRefinedBy Qs)] =
       r match
         case r: LiftObj[ps, qs] =>
           Indeed((summon[T =:= Obj[qs]], r.aRb))
-        case Trans(r, s) =>
-          ???
 
   extension [Ps, Qs](r: Obj[Ps] IsRefinedBy Obj[Qs])
     def unliftObj: Ps IsRefinedBy Qs =
       r match
         case LiftObj(aRb) =>
           aRb
-        case Trans(aRb, bRc) =>
-          ???
 
   extension [C, T](r: Const[C] IsRefinedBy T)
     def preservesConst: Exists[[D] =>> (T =:= Const[D], C IsRefinedBy D)] =
       r match
         case r: LiftConst[c, d] =>
           Indeed((summon[T =:= Const[d]], r.aRb))
-        case Trans(r, s) =>
-          ???
 
   extension [A, Cs, T](r: Enum[A, Cs] IsRefinedBy T)
     def preservesEnum: Exists[[B] =>> Exists[[Ds] =>> (T =:= Enum[B, Ds], A IsRefinedBy B, Cs IsRefinedBy Ds)]] =
       r match
         case r: LiftEnum[a, b, cs, ds] =>
           Indeed(Indeed((summon[T =:= Enum[b, ds]], r.aRb, r.cRd)))
-        case Trans(aRb, bRc) =>
-          ???
 
   extension [S <: String, T](r: S IsRefinedBy T)
     def isStringRefl: S =:= T =
       r match
         case LiftSingletonString(_) =>
           summon[S =:= T]
-        case Trans(aRb, bRc) =>
-          ???
+        case LiftSingletonInt(s) =>
+          summon[S =:= T]
+          sys.error(s"Absurd: ${s.value: Int & String}: Int & String")
+        case LiftSingletonLong(s) =>
+          summon[S =:= T]
+          sys.error(s"Absurd: ${s.value: Long & String}: Long & String")
+        case LiftSingletonBoolean(s) =>
+          summon[S =:= T]
+          sys.error(s"Absurd: ${s.value: Boolean & String}: Boolean & String")
 
   extension [T](r: Str IsRefinedBy T)
     def isStrRefl: Str =:= T =
       r match
         case LiftStr =>
           summon[Str =:= T]
-        case Trans(aRb, bRc) =>
-          ???
 
   extension [T](r: Void IsRefinedBy T)
     def isVoidRefl: Void =:= T =
       r match
         case LiftVoid =>
           summon[Void =:= T]
-        case Trans(aRb, bRc) =>
-          ???
 
 }
